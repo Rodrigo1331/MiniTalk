ad03239af76db01009899512320d885873feac6a
Rodrigo Cruz Antunes <rcruz-an@c1r10s12.42porto.com>
2023-05-05 14:45:44 +0100

    client and server updates

--- client.c
+++ client.c
@@ -6,7 +6,44 @@
 /*   By: rcruz-an <rcruz-an@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 09:23:37 by rcruz-an          #+#    #+#             */
-/*   Updated: 2023/05/05 09:23:38 by rcruz-an         ###   ########.fr       */
+/*   Updated: 2023/05/05 14:23:11 by rcruz-an         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+void	message_to_server(int pid, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (1 << bit)) == 0)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(50);
+		bit++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int		pid;
+	int		i;
+
+	i = 0;
+	if (ac != 3)
+		ft_printf("Error!\nYou should have a process ID and a message as inputs\n");
+	else
+	{
+		pid = ft_atoi(av[1]);
+		while (av[2][i])
+		{
+			message_to_server(pid, av[2][i]);
+			i++;
+		}
+		message_to_server(pid, '\n');
+	}
+}
--- server.c
+++ server.c
@@ -6,7 +6,44 @@
 /*   By: rcruz-an <rcruz-an@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 09:08:14 by rcruz-an          #+#    #+#             */
-/*   Updated: 2023/05/05 09:08:15 by rcruz-an         ###   ########.fr       */
+/*   Updated: 2023/05/05 14:38:38 by rcruz-an         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+void	handler(int signal)
+{
+	static int	c;
+	static int	bit = 0;
+
+	if (signal == SIGUSR1)
+		c |= 1 << bit;
+	bit++;
+	if (bit == 8)
+	{
+		write(1, &c, 1);
+		bit = 0;
+		c = 0;
+	}
+}
+
+int main (int ac, char **av)
+{
+	struct sigaction sa;
+
+	sa.sa_handler = &handler;
+	(void)av;
+	if (ac != 1)
+	{
+		ft_printf("error\nTry to use: ./server\n");
+		return (0);
+	}
+	ft_printf("This is my pid: %d\n", getpid());
+	while (1)
+	{
+		sigaction(SIGUSR1, &action, NULL);
+		sigaction(SIGUSR2, &action, NULL);
+		pause();
+	}
+}
